901416c6fcf65d0560eae10a6eca65c4a9748255
Ernesto Pang <ernestopang513@hotmail.com>
2021-04-22 00:47:14 -0500

    Primer commit
    Se establecieron las rutas de al app y la redirección de rutas erroneas

---
+++ src/router/AppRouter.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    BrowserRouter,
+    Redirect
+} from "react-router-dom";
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route
+                        path = '/'
+                        exact
+                        component = {CalendarScreen}
+                    />
+                    <Route
+                        path = '/login'
+                        exact
+                        component = {LoginScreen}
+                    />
+
+                    <Redirect 
+                        to = '/'
+                    />
+                    
+                    
+                </Switch>
+            </div>     
+        </Router>
+    )
+}

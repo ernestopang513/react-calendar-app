e47d2a85d6c37b7958cd254a5b6039016b08a486
Ernesto Pang <ernestopang513@hotmail.com>
2021-04-25 22:44:51 -0500

    funciono

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-import DateTimePicker from 'react-datetime-picker';
 import moment from 'moment';
+import { MuiPickersUtilsProvider, KeyboardDateTimePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 const customStyles = {
     content : {
         top                   : '50%',
@@ -66,78 +67,74 @@ export const CalendarModal = () => {
         }
     }
     return (
-        <Modal
-            isOpen={true}
-            onRequestClose={closeModal}
-            style={customStyles}
-            closeTimeoutMS = {200}
-            className = 'modal'
-            overlayClassName = 'modal-fondo'
-        >
-
-            <h1> Nuevo evento </h1>
-            <hr />
-            <form 
-                className="container"
-                onSubmit = {handleSubmit}
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Modal
+                isOpen={true}
+                onRequestClose={closeModal}
+                style={customStyles}
+                closeTimeoutMS={200}
+                className="modal"
+                overlayClassName="modal-fondo"
             >
-
-                <div className="form-group">
-                    <label>Fecha y hora inicio</label>
-                    <DateTimePicker
-                        onChange={handleStartDateChange}
-                        value={dateStart}
-                        className = 'form-control'
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label>Fecha y hora fin</label>
-                    <DateTimePicker
-                        onChange={handleEndDateChange}
-                        value={dateEnd}
-                        minDate = {dateStart}
-                        className = 'form-control'
-                    />
-                </div>
-
+                <h1>Nuevo evento</h1>
                 <hr />
-                <div className="form-group">
-                    <label>Titulo y notas</label>
-                    <input 
-                        type="text" 
-                        className="form-control"
-                        placeholder="Título del evento"
-                        name="title"
-                        value = {title}
-                        autoComplete="off"
-                        onChange = {handleInputChange}
-                    />
-                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
-                </div>
-
-                <div className="form-group">
-                    <textarea 
-                        type="text" 
-                        className="form-control"
-                        placeholder="Notas"
-                        rows="5"
-                        name="notes"
-                        onChange = {handleInputChange}
-                        value = {notes}
-                    ></textarea>
-                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
-                </div>
-
-                <button
-                    type="submit"
-                    className="btn btn-outline-primary btn-block"
-                >
-                    <i className="far fa-save"></i>
-                    <span> Guardar</span>
-                </button>
-
-            </form>
-        </Modal>
-    )
+                <form className="container" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <KeyboardDateTimePicker
+                            inputVariant="outlined"
+                            ampm={false}
+                            label="Fecha y hora inicio"
+                            value={dateStart}
+                            onChange={handleStartDateChange}
+                            format="MMMM dd yyyy, HH:mm "
+                        />
+                    </div>
+                    <div className="form-group">
+                        <KeyboardDateTimePicker
+                            inputVariant="outlined"
+                            ampm={false}
+                            label="Fecha y hora fin"
+                            value={dateEnd}
+                            onChange={handleEndDateChange}
+                            minDate={dateStart}
+                            minDateMessage="End Date should be at least start Date "
+                            format="MMMM dd yyyy, HH:mm "
+                        />
+                    </div>
+                    <hr />
+                    <div className="form-group">
+                        <label>Titulo y notas</label>
+                        <input
+                            type="text"
+                            className={`form-control`}
+                            placeholder="Título del evento"
+                            name="title"
+                            value={title}
+                            onChange={handleInputChange}
+                            autoComplete="off" />
+                        <small id="emailHelp" className="form-text text-muted"> Una descripción corta </small>
+                    </div>
+                    <div className="form-group">
+                        <textarea
+                            type="text"
+                            className="form-control"
+                            placeholder="Notas"
+                            rows="5"
+                            name="notes"
+                            value={notes}
+                            onChange={handleInputChange}
+                        >
+                        </textarea>
+                        <small id="emailHelp" className="form-text text-muted"> Información adicional </small>
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn btn-outline-primary btn-block"
+                    ><i className="far fa-save"></i>
+                        <span> Guardar</span>
+                    </button>
+                </form>
+            </Modal>
+        </MuiPickersUtilsProvider>
+    );
 }
